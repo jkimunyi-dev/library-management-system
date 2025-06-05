3a93860249d278e6032302fb0e4f969ad33b565c
Jimmy Kimunyi <jimmy@alluvium.net>
2025-06-05 09:12:08 +0300

    Members  modules, controllers, and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
+import { MembersModule } from './members/members.module';
 
 @Module({
-  imports: [AuthorsModule, BooksModule],
+  imports: [AuthorsModule, BooksModule, MembersModule],
   controllers: [AppController],
   providers: [AppService],
 })
